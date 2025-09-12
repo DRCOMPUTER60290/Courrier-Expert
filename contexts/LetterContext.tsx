12fceae79a4a111129edf1a102edd6c8f3be9625
DRCOMPUTER60290 <drcomputer60290@gmail.com>
2025-09-12 18:20:39 +0200

    feat: add notification service

--- contexts/LetterContext.tsx
+++ contexts/LetterContext.tsx
@@ -10,6 +10,8 @@ export interface Letter {
   recipient: Recipient;
   data: Record<string, any>;
   createdAt: Date;
+  reminderDate?: Date;
+  notificationId?: string;
 }
 
 interface LetterContextType {
